7f529198d1d1697679d9303b117736bf37169116
tyrantdavis <davistyrant@gmail.com>
2017-02-13 16:10:36 -0600

    In progress Wiki CRUD

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'random_data'
+
+5.times do
+    user= User.new(
+        email: RandomData.random_email,
+        password: "password",
+        )
+    user.save!
+end
+
+users = User.all
+
+10.times do
+    Wiki.create!(
+        title: RandomData.random_sentence,
+        body: RandomData.random_paragraph,
+        private: false,
+        user: User.last
+        )
+end
+
+wikis = Wiki.all
+
+puts "Seed finished"
+puts "#{users.count} users created"
+puts "#{wikis.count} wikis created"
