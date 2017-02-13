544ea3857de8c00fcf0473c7d9cf75e9e4bcc50d
tyrantdavis <davistyrant@gmail.com>
2017-02-13 14:32:36 -0600

    Create Wikis controller, refactor routes.rb, update path helpers application.html.erb, delete app/models/.keep file

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :wikis
 
   get 'welcome/index'
-
-  get 'welcome/about'
-
+  get 'about' => 'welcome#about'
   root to: 'welcome#index'
 end
