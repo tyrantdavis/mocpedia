ce676407fad9e09ae2a8c25d9f38c9bba2af25c5
tyrantdavis <davistyrant@gmail.com>
2017-02-19 12:08:11 -0600

    Activate Devise modules, edit users migration file

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,11 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
 
-  # Delete wikis owned by user when user is deleted. 
+  # Delete wikis owned by user when user is deleted.
   has_many :wikis, dependent: :destroy
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable, :lockable
+[...]
 end
