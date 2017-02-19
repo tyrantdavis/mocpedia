b6cd9334b277ef5c1d98a8f1798da0613238d081
tyrantdavis <davistyrant@gmail.com>
2017-02-19 12:14:34 -0600

    Edit user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable, :lockable
-[...]
+
 end
