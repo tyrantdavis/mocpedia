05c4119d431dd23427ee65d8ee4b8b3df2dc4a8d
tyrantdavis <davistyrant@gmail.com>
2017-02-22 12:51:17 -0600

    Set premium user Charges controller

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -11,10 +11,12 @@ class ChargesController < ApplicationController
      # Where the real magic happens
      charge = Stripe::Charge.create(
        customer: customer.id, # Note -- this is NOT the user_id in your app
-       amount: Amount.default,
-       description: "BigMoney Membership - #{current_user.email}",
+       amount: Amount.15_00,
+       description: "Blocipedia Premium Membership - #{current_user.email}",
        currency: 'usd'
      )
+
+     current_user.premium!
 
      flash[:notice] = "Thanks for all the money, #{current_user.email}! Feel free to pay me again."
      redirect_to user_path(current_user) # or wherever
