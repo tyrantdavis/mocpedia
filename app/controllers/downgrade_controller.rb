6a52427cedf6867c8036c8315feaf9fd1781ab35
tyrantdavis <davistyrant@gmail.com>
2017-02-24 10:14:47 -0600

    Update user role, align block statement, reposition comment

--- app/controllers/downgrade_controller.rb
+++ app/controllers/downgrade_controller.rb
@@ -1,14 +1,18 @@
 class DowngradeController < ApplicationController
   def new
   end
-  # Change user role to standard then...
+
   def create
-    current_user.standard?
+    # Change user role to standard then...
+    current_user.standard!
+
     @wikis = current_user.wikis
+
     @wikis.each do |wiki|
-      # ... make private wikis to public.
-        wiki.update_attribute(:private, false)
-      end
+      # ... make private wikis public.
+      wiki.update_attribute(:private, false)
+    end
+
   end
 
   flash[:notice] = "You have sucessfully downgraded. All of your private wikis are now available for public viewing."
