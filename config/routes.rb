ef8e78abeb89534f50c0cc0f3df991e384001144
tyrantdavis <davistyrant@gmail.com>
2017-02-28 11:19:58 -0600

    Define a different root route(secret- index) for logged in out users

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
-  get 'downgrade/new'
 
   devise_for :users
   resources :wikis
   resources :charges, only: [:new, :create]
   resources :downgrade, only: [:new, :create]
+  resources :secret, only: [:index]
 
+  authenticated :user do
+    root 'secret#index', as: :authenticated_root
+  end
+
+  get 'secret/index'
+  get 'downgrade/new'
   get 'welcome/index'
   get 'about' => 'welcome#about'
   root to: 'welcome#index'
