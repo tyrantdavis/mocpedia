ed5cd05a50bd0a6805a7c383e39c37856e806649
tyrantdavis <davistyrant@gmail.com>
2017-03-02 14:04:18 -0600

    Add markdown class and renderer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def markdown_text(object)
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, filter_html: true, no_images: true, no_styles: true, safe_links_only: true, with_toc_data: true, prettify: true, hard_wrap: true)
+
+    (markdown.render object).html_safe
+  end
 end
