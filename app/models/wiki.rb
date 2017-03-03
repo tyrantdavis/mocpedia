8f28bc00ceb03aad1f1bdd9a8cecbcefd6bf6fcb
tyrantdavis <davistyrant@gmail.com>
2017-03-02 18:26:30 -0600

    Add wiki validations.

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -1,8 +1,12 @@
 class Wiki < ActiveRecord::Base
   belongs_to :user
+
   has_many :collaborators
   has_many :users, through: :collaborators
 
   scope :visible_to, -> (user) { user && ((user.admin?) || (user.premium?)) ? all : where(private: false)}
+
+  validates :title, presence: true, length: { minimum: 5 }
+  validates :body, presence: true, length: { minimum: 20 }
 
 end
