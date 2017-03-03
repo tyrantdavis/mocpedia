e0f752423f1c09904c3cd10d03888b21ea7ce817
tyrantdavis <davistyrant@gmail.com>
2017-03-03 15:50:23 -0600

    Update routes, schema, and seeds. Drop, recreate and seed db.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,27 @@
 require 'random_data'
 
-5.times do
-    user = User.new(
-        name: RandomData.random_name,
-        email: RandomData.random_email,
-        password: "password"
-        )
-    user.save!
+puts "**  Seeding Database: seeding ***\n\n"
+
+unless User.where(email: "admin@example.com").first
+  User.create!(name: "Admin", email: 'admin@example.com',
+          password: '123123',
+          password_confirmation: '123123')
+
+          puts "-- Created admin@example.com with password 123123\n"
 end
+
+            50.times do
+              user = User.new(
+              name: RandomData.random_name,
+              email: RandomData.random_email,
+              password: "password"
+              )
+    user.save!
+  end
 
 users = User.all
 
-10.times do
+50.times do
     Wiki.create!(
         title: RandomData.random_sentence,
         body: RandomData.random_paragraph,
@@ -25,3 +35,4 @@ wikis = Wiki.all
 puts "Seed finished"
 puts "#{users.count} users created"
 puts "#{wikis.count} wikis created"
+puts "\n**  Seeding Database: completed ***"
