fd2c9b29ed280448e158a0e40eb56decc981853c
tyrantdavis <davistyrant@gmail.com>
2017-03-06 10:00:25 -0600

    Setup collaborator model relationships for user.rb and wiki.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
 
   # Delete wikis owned by user when user is deleted.
   has_many :wikis, dependent: :destroy
+  has_many :collaborators
+  has_many :wikis, through: :collaborators, source: :wiki
 
   before_save { self.role ||= :standard }
 
