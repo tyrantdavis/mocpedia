02d95d1750bd5b0fe21751049e259a8a8af2dddb
Tyrant Davis <davistyrant@gmail.com>
2018-12-12 15:53:08 -0600

    Update user wikis index.

--- app/controllers/secret_controller.rb
+++ app/controllers/secret_controller.rb
@@ -2,5 +2,6 @@ class SecretController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @wikis = current_user.wikis.paginate(page: params[:page], per_page: 9)
   end
 end
